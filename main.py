0a7d3fc480b324fb6a0bd00ffe46efe53d23c968
庄楚辉 <zch@aicloud-edu.com>
2022-10-18 17:37:39 +0800

    feat: 新增配置

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 
 
 from enum import Enum
-from typing import Union
-from pydantic import  BaseModel
-from fastapi import FastAPI
+from typing import Union,List
+from pydantic import BaseModel
+from fastapi import FastAPI, Query, Path
 
 app = FastAPI()
 
@@ -79,4 +79,81 @@ async def create_item(item_id: int, i: Item, q: Union[str, None] = None):
     result = {"item_id": item_id, **i.dict()}
     if q:
         result.update({"q": q})
-    return result;
+    return result
+
+
+# 查询参数 & 字符串校验
+# Query 可以用于查询参数的校验
+# q: Union[str, None] = Query(default=None) 等于 q: str = None
+@app.get("/books/")
+async def get_books(q: Union[str, None] = Query(default=None, min_length=5, max_length=50)):
+    result = {
+        "book_name": "《聪明的个人成长》",
+        "q": q
+    }
+    return result
+
+
+# 接收一个数组参数
+# 实例：/books_list?q=1&q=22&q=33
+# q: ["1","22","33"]
+@app.get("/books_list")
+async def get_books(
+    q: Union[List[str], None] = Query(default=None, min_length=3, )):
+    query_items = {"q": q}
+    return query_items
+
+
+
+# 路径校验  Path
+# /users/21?item-query=2
+@app.get("/users/{user_id}")
+async def get_users(
+    user_id: int = Path(title="The ID of the item to get"),
+    q: Union[str, None] = Query(default=None, alias="item-query"),
+):
+    results = {"item_id": user_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+
+class Book(BaseModel):
+    title:str
+    price:int
+    # description: Union[str, None] = None,
+    page_size:int
+    
+
+# put 多个参数
+@app.put('/book/{book_id}')
+async def update_book(
+    *,
+    book_id:int = Path(title='The ID of the item to get', ge=0, le=1000),
+    q:Union[str, None] = None,
+    book:Union[Book,None] = None,
+):
+    results = {"book_id": book_id}
+    if q:
+        results.update({"q":q})
+    if book:
+        results.update({"book":book})
+    return results
+
+
+# put 多个参数
+# 使用 
+@app.put('/book2/{book_id}')
+async def update_book(
+    *,
+    book_id:int = Path(title='The ID of the item to get', ge=0, le=1000),
+    q:Union[str, None] = None,
+    book:Union[Book,None] = None,
+):
+    results = {"book_id": book_id}
+    if q:
+        results.update({"q":q})
+    if book:
+        results.update({"book":book})
+    return results
